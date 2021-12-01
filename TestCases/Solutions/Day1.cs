1bb6b68d942f989321c95a82b2bc64ec2e818f96
ralcaraz <ralcarazerhs@gmail.com>
2021-12-01 11:47:32 -0800

    Fixed missing inputs

--- TestCases/Solutions/Day1.cs
+++ TestCases/Solutions/Day1.cs
@@ -7,12 +7,13 @@ namespace TestCases.Solutions;
 
 public class Day1
 {
-    private readonly double[] _nums = File.ReadAllLines("Inputs/Day1Input.txt")
+    private readonly double[] _nums = File.ReadAllLines("Inputs/Day1.txt")
+        .Where(s => !string.IsNullOrWhiteSpace(s))
         .Select(double.Parse)
         .ToArray();
 
     [Test]
-    public void CountNumIncreases_ShouldPass()
+    public void CountNumIncreases_ShouldReturnAnswer()
     {
         var increaseCount = _nums.CountIncreases();
 
@@ -20,7 +21,7 @@ public class Day1
     }
 
     [Test]
-    public void SlidingWindowNumIncreases_ShouldPass()
+    public void SlidingWindowNumIncreases_ShouldReturnAnswer()
     {
         var increaseCount = _nums
             .GetSlidingWindowAverages(3)
